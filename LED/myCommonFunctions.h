c68838f3ad0cf2d9927df1f29ed80ddea4cfbb38
theopin <theopinto98@gmail.com>
2020-09-24 13:18:31 +0800

    Add ability to toggle red and green LED

---
+++ LED/myCommonFunctions.h
@@ -0,0 +1,22 @@
+# include <MKL25Z4.h>
+
+# define MASK(x) (1 << (x))
+
+static void delay(volatile uint32_t nof) {
+  while(nof!=0) {
+    __asm("NOP");
+    nof--;
+  }
+}
+
+void initGPIOPin(__IO uint32_t *PCR, __IO uint32_t *PDDR, int pin, int isOutput)
+{
+    *PCR &= ~PORT_PCR_MUX_MASK;
+    *PCR |= PORT_PCR_MUX(1);
+    
+    if(isOutput)
+        *PDDR |= (MASK(pin));
+    // Input
+    else
+        *PDDR &= ~(MASK(pin));
+}
